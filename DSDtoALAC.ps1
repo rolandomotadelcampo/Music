2221b7466f9ca1312fcdeee25d13aabf71355236
alfred <aalaredo@miners.utep.edu>
2022-04-27 00:04:55 -0600

    Add Output directory
    Signed-off-by: alfred <aalaredo@miners.utep.edu>

--- DSDtoALAC.ps1
+++ DSDtoALAC.ps1
@@ -1,10 +1,13 @@
-$Working_Directory = Read-Host -Prompt "Enter the working directory: "
+$Working_Directory = Read-Host -Prompt "Enter the working directory: " 
+$Output_Directory = Read-Host -Prompt "Enter the output directory: " 
 
 Set-Location $Working_Directory
 
-$DSD_Tracks = get-childitem -path "*" -recurse -filter "*.dsf"
+$DSD_Tracks = get-childitem -path "*" -recurse -filter "*.dsf" # Get all .dsf files in the working directory
 
-foreach ($song in $DSD_Tracks) {
-    $outputSong = [System.IO.Path]::ChangeExtension($song.FullName, ".m4a")
-    ffmpeg.exe -i $song.FullName -acodec alac -ar 88200 $outputSong
+foreach ($song in $DSD_Tracks) { #loop through all the tracks
+    $outputSong = [System.IO.Path]::ChangeExtension($song.FullName, ".m4a") # create the output file name
+    ffmpeg.exe -i $song.FullName -acodec alac -ar 88200 $outputSong # convert the track
 }
+
+Get-Item -Path $Working_Directory -Recurse -Filter "*.m4a" | Move-Item -Destination $Output_Directory # move the converted files to the output directory
